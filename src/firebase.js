24bb5b105c12db211975f339ac6414c0e93c9181
Ronaldo Aquino <ronaldoaquinodev@gmail.com>
2021-01-15 20:01:42 -0300

    Projeto iniciado e adicionado o firebase

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBwtqk2AWDz9__0NSm4CSupbfvo2uJrRKo",
+  authDomain: "task-app-redux.firebaseapp.com",
+  projectId: "task-app-redux",
+  storageBucket: "task-app-redux.appspot.com",
+  messagingSenderId: "651577981096",
+  appId: "1:651577981096:web:5e2fae4e4bdd0044392a9d"
+};
