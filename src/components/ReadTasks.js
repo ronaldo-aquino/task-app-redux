ccaa30c7ee9b85c34afb5472e5ba44d18c369ef6
Ronaldo Aquino <ronaldoaquinodev@gmail.com>
2021-01-18 11:45:00 -0300

    Deixando a tabela responsiva

--- src/components/ReadTasks.js
+++ src/components/ReadTasks.js
@@ -40,60 +40,62 @@ const ReadTask = () => {
           Add Tarefa
         </button>
       </div>
-      <table className="table mt-3">
-        <thead>
-          <tr>
-            <th scope="col">Título</th>
-            <th scope="col">Descrição</th>
-            <th scope="col">Finalizado</th>
-            <th scope="col">Ações</th>
-          </tr>
-        </thead>
-        <tbody>
-          {tasks &&
-            tasks.map((task) => (
-              <tr key={task.id}>
-                <td className={task.finished ? "finished-task" : undefined}>
-                  {task.title}
-                </td>
-                <td className={task.finished ? "finished-task" : undefined}>
-                  {task.description}
-                </td>
-                <td>
-                  {task.finished ? (
-                    <span className="badge badge-success p-1">SIM</span>
-                  ) : (
-                    <span className="badge badge-info p-1">NÃO</span>
-                  )}
-                </td>
-                <td>
-                  <div>
-                    <button
-                      className="btn btn-sm btn-dark mr-2"
-                      onClick={() => dispatch(finishedTask(task.id))}
-                      disabled={task.finished}
-                    >
-                      Finalizar
-                    </button>
-                    <button
-                      className="btn btn-sm btn-warning mr-2"
-                      disabled={task.finished}
-                      onClick={() => editMode(task)}
-                    >
-                      Editar
-                    </button>
-                    <button
-                      className="btn btn-sm btn-danger"
-                      onClick={() => dispatch(deleteTask(task.id))}
-                    >
-                      Excluir
-                    </button>
-                  </div>
-                </td>
-              </tr>
-            ))}
-        </tbody>
-      </table>
+      <div className="table-responsive mt-3">
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Título</th>
+              <th scope="col">Descrição</th>
+              <th scope="col">Finalizado</th>
+              <th scope="col">Ações</th>
+            </tr>
+          </thead>
+          <tbody>
+            {tasks &&
+              tasks.map((task) => (
+                <tr key={task.id}>
+                  <td className={task.finished ? "finished-task" : undefined}>
+                    {task.title}
+                  </td>
+                  <td className={task.finished ? "finished-task" : undefined}>
+                    {task.description}
+                  </td>
+                  <td>
+                    {task.finished ? (
+                      <span className="badge badge-success p-1">SIM</span>
+                    ) : (
+                      <span className="badge badge-info p-1">NÃO</span>
+                    )}
+                  </td>
+                  <td>
+                    <div>
+                      <button
+                        className="btn btn-sm btn-dark mr-2"
+                        onClick={() => dispatch(finishedTask(task.id))}
+                        disabled={task.finished}
+                      >
+                        Finalizar
+                      </button>
+                      <button
+                        className="btn btn-sm btn-warning mr-2"
+                        disabled={task.finished}
+                        onClick={() => editMode(task)}
+                      >
+                        Editar
+                      </button>
+                      <button
+                        className="btn btn-sm btn-danger"
+                        onClick={() => dispatch(deleteTask(task.id))}
+                      >
+                        Excluir
+                      </button>
+                    </div>
+                  </td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      </div>
     </>
   );
 };
